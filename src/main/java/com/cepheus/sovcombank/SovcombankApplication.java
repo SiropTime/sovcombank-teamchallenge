e951851c101738d03f7dadb52400c5bc4615d26c
dmitriyDenisenko <dipster86@yandex.ru>
2022-11-19 01:42:02 +0300

    Generate: create empty prod

---
+++ src/main/java/com/cepheus/sovcombank/SovcombankApplication.java
@@ -0,0 +1,13 @@
+package com.cepheus.sovcombank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovcombankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovcombankApplication.class, args);
+	}
+
+}
