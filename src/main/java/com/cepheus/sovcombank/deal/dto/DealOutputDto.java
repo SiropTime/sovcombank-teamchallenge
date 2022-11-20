97a8c9ecde482f5c40bfe74bc20512656eab80e7
dmitriyDenisenko <dipster86@yandex.ru>
2022-11-20 09:59:32 +0300

    Refactor: balance create

--- src/main/java/com/cepheus/sovcombank/deal/dto/DealOutputDto.java
+++ src/main/java/com/cepheus/sovcombank/deal/dto/DealOutputDto.java
@@ -2,10 +2,14 @@ package com.cepheus.sovcombank.deal.dto;
 
 
 import com.cepheus.sovcombank.deal.model.Operation;
-import lombok.Builder;
+import lombok.*;
 
 import java.time.LocalDateTime;
 @Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DealOutputDto {
 
     private LocalDateTime timeStamp;
